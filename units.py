1d30d3dbf6bd6f65d58fe1f24fd6447845260ec9
jeffcodez <vtjeff@gmail.com>
2023-07-14 09:39:28 -0700

    Add files via upload

---
+++ units.py
@@ -0,0 +1,41 @@
+class C():
+    from math import pi as mypi
+    # angles
+    DEG = mypi/180.
+    ARCMIN = mypi/180/60
+    ARCSEC = mypi/180/360
+    RAD = 1.
+
+    # mass
+    SLUG = 1.
+    SLINCH = 12.
+    KG = 1/14.5939
+    GM = 1/14593.9
+
+    # length
+    FT = 1.
+    IN = 1./12.
+    M = 1000./25.4/12.
+    CM = 10/25.4/12
+    MM = 1/25.4/12
+
+    # time
+    SEC = 1.
+    MILISEC = 1.0e-3
+    MICROSEC = 1.0e-6
+
+    # force
+    LBF = 1.
+    N = 0.22480894387096
+    KN = 224.80894387096
+
+    # gravitational constant
+    G0 = 32.174
+    LBM = 1/32.174
+
+    # pressure
+    PSI = 144.
+    KSI = 144.0e3
+    MSI = 144.0e6
+    PSF = 1.
+    PA = 20.89/1000
