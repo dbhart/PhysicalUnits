7dcb7af804b20804daadcae9d4ccc9ccf0a8502b
jeffcodez <vtjeff@gmail.com>
2023-09-26 15:35:36 -0700

    added angular rate, scrubbed all calcs to be explicity floating point, added comments related to reference units

--- units.py
+++ units.py
@@ -1,5 +1,5 @@
 # PyUnits
-# Physical units for scientific and engineering This is a simple implementation of a units framework for python, and can be expanded upon as desired.
+# Physical units for scientific and engineering This is a simple implementation of a units framework for python, and can be expanded upon as desired. These conversions are implemented in an easy-to-read format, which may add additional computation time, in python, when used in operations that involve many computations.
 # Use:
 # from units import C
 # assign units to a number - in this case 10 lbf of force:
@@ -15,58 +15,64 @@
 
 class C():
     from math import pi
-    # angles
+    # angles - ref radians
     DEG = pi/180.
-    ARCMIN = pi/180/60
-    ARCSEC = pi/180/360
+    ARCMIN = pi/180./60.
+    ARCSEC = pi/180./360.
     RAD = 1.
+    
+    #periodic rate ref - rad/sec
+    HZ = 1./2./pi
+    DPS = 180./pi
+    RPM = 60./2./pi
 
-    # mass
+    # mass - ref slug
     SLUG = 1.
     SLINCH = 12.
-    KG = 1/14.5939
-    GM = 1/14593.9
+    KG = 1./14.5939
+    GM = 1./14593.9
 
-    # length
+    # length - ref ft
     FT = 1.
     IN = 1./12.
     M = 1000./25.4/12.
-    CM = 10/25.4/12
-    MM = 1/25.4/12
+    CM = 10./25.4/12.
+    MM = 1./25.4/12.
     MILE = 5280.
-    NM = 6076
+    NM = 6076.
 
-    # time
+    # time - ref sec
     SEC = 1.
     MILISEC = 1.0e-3
     MICROSEC = 1.0e-6
     MIN = 60.
     HOUR = 3600.
-    DAY = 3600*24.
-    YEAR = 3600*24.*365
+    DAY = 3600.*24.
+    YEAR = 3600.*24.*365.
 
-    # speed
+    # speed - ref fps
     FPS = 1.
     MPH = 5280./3600.
     KNOT = 6076./3600.
 
-    # force
+    # force - ref pound-force
     LBF = 1.
     N = 0.22480894387096
     KN = 224.80894387096
 
-    # gravitational constant
+    # gravitational constant - ref earth surface gravity in ft/sec2
     G0 = 32.174
-    LBM = 1/32.174
+    LBM = 1./32.174
 
-    # pressure
+    # pressure - ref lbf/ft2
     PSI = 144.
     KSI = 144.0e3
     MSI = 144.0e6
     PSF = 1.
-    PA = 20.89/1000
+    PA = 20.89/1000.
 
-    # # temperature
+    # temperature !!To be later implemented - this unit requires a DC offset, which would require a modification of
+    # python std operotors to implement in its given syntax!!
     # K = -273.15
     # C = 1.
     # F = 9./5.+32.
