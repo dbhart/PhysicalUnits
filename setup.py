3f05400c862da662b809e64b10ab74366c349d19
jeffcodez <vtjeff@gmail.com>
2024-03-18 12:39:06 -0600

    rename

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='physicalunits',
+    name='simple-unit-system',
     version='0.0.2',
+    description="A framework for applying physical units and performing unit conversions, for scientific and engineering analyses, based on NIST conversions and constants",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
     ],
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
     python_requires=">=3, <4",
     project_urls={  # Optional
         "Bug Reports": "https://github.com/jeffcodez/PhysicalUnits/issues",
