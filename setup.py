48f0edd401926fd0efd84cfa61f95982d9b8270c
jeffcodez <vtjeff@gmail.com>
2024-03-18 14:13:17 -0600

    First Release

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='simple-unit-system',
-    version='0.0.3',
+    version='0.0.5',
     description="A framework for applying physical units and performing unit conversions, for scientific and engineering analyses, based on NIST conversions and constants",
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     install_requires=[
     ],
+    keywords="unit","units","physical","system","SI","NIST","engineering","scientific","measurements","weights","physics",
     package_dir={"": "src"},
     python_requires=">=3, <4",
     project_urls={  # Optional
